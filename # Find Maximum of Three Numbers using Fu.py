ae45d3bfd2d658f9dcd7d7cfaf900057c86be56d
Aditi <aditisinghal740@gmail.com>
2026-02-25 14:57:48 +0530

    Add files via upload

---
+++ # Find Maximum of Three Numbers using Fu.py
@@ -0,0 +1,11 @@
+# Find Maximum of Three Numbers using Function
+
+def maximum(a, b, c):
+    return max(a, b, c)
+
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+print("Maximum =", maximum(x, y, z))
