0639f471ed1faac78992ac9033bd9110eea32dca
Aditi <aditisinghal740@gmail.com>
2026-02-25 10:54:45 +0530

    Add files via upload

---
+++ # Read inputs.py
@@ -0,0 +1,10 @@
+# Read inputs
+p = float(input())  # Principal
+r = float(input())  # Rate
+t = float(input())  # Time
+
+# Calculate SI
+si = (p * r * t) / 100
+
+# Print result
+print("Simple Interest:", si)
