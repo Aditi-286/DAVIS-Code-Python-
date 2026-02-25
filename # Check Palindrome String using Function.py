e27bf006ea7101452b20648ecdb03b68d94ee904
Aditi <aditisinghal740@gmail.com>
2026-02-25 14:57:07 +0530

    Add files via upload

---
+++ # Check Palindrome String using Function.py
@@ -0,0 +1,12 @@
+# Check Palindrome String using Function
+
+def is_palindrome(text):
+    return text == text[::-1]
+
+
+s = input("Enter a string: ")
+
+if is_palindrome(s):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
