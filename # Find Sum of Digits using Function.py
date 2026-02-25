765c6a637ee4696fd490f3f8ea660f8cfd526139
Aditi <aditisinghal740@gmail.com>
2026-02-25 14:59:12 +0530

    Add files via upload

---
+++ # Find Sum of Digits using Function.py
@@ -0,0 +1,15 @@
+# Find Sum of Digits using Function
+
+def sum_of_digits(n):
+    total = 0
+
+    while n > 0:
+        total += n % 10
+        n //= 10
+
+    return total
+
+
+num = int(input("Enter a number: "))
+
+print("Sum of digits =", sum_of_digits(num))
