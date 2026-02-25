90c610db8f6faf3fe10b90298d1f7327cf356fcc
Aditi <aditisinghal740@gmail.com>
2026-02-25 15:02:46 +0530

    Add files via upload

---
+++ # Find GCD using Function.py
@@ -0,0 +1,12 @@
+# Find GCD using Function
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("GCD =", gcd(x, y))
