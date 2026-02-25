3be2a94d3fc2ab9f871b32e3ffccd3c70ec0ae3d
Aditi <aditisinghal740@gmail.com>
2026-02-25 11:40:35 +0530

    Add files via upload

---
+++ # Read four numbers.py
@@ -0,0 +1,11 @@
+# Read four numbers
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+
+# Find greatest
+greatest = max(a, b, c, d)
+
+# Print result
+print("Greatest number:", greatest)
