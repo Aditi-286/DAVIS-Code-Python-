6b76d97ddb2b036c49cfe6e5b2cfe40fd876db62
Aditi <aditisinghal740@gmail.com>
2026-02-25 11:20:58 +0530

    Add files via upload

--- # Read number.py
+++ # Read number.py
@@ -1,8 +1,11 @@
 # Read number
 num = int(input())
 
-# Check using bitwise operator
-if num & 1 == 0:
-    print("Even")
-else:
-    print("Odd")
+sum_digits = 0
+n = abs(num)  # Handle negative numbers
+
+while n > 0:
+    sum_digits += n % 10
+    n //= 10
+
+print(sum_digits)
