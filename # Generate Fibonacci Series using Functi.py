0a5ea3c1d1832edf1471dcd1168e33e36335a186
Aditi <aditisinghal740@gmail.com>
2026-02-25 14:59:51 +0530

    Add files via upload

---
+++ # Generate Fibonacci Series using Functi.py
@@ -0,0 +1,14 @@
+# Generate Fibonacci Series using Function
+
+def fibonacci(n):
+    a = 0
+    b = 1
+
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+
+terms = int(input("Enter number of terms: "))
+
+fibonacci(terms)
