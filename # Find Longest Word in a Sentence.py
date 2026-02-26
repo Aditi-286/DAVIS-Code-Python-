3adb35d762c97c771960e11dbfb8c94d4e8be974
Aditi <aditisinghal740@gmail.com>
2026-02-26 13:43:29 +0530

    Add files via upload

---
+++ # Find Longest Word in a Sentence.py
@@ -0,0 +1,13 @@
+# Find Longest Word in a Sentence
+
+text = input("Enter a sentence: ")
+
+words = text.split()
+
+longest = ""
+
+for w in words:
+    if len(w) > len(longest):
+        longest = w
+
+print("Longest word =", longest)
