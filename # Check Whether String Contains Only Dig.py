4d589061bc1f4ef04944ed5d2b0e185c7a086d23
Aditi <aditisinghal740@gmail.com>
2026-02-26 14:02:53 +0530

    Add files via upload

---
+++ # Check Whether String Contains Only Dig.py
@@ -0,0 +1,15 @@
+# Check Whether String Contains Only Digits
+
+text = input("Enter a string: ")
+
+is_digit = True
+
+for ch in text:
+    if ch < '0' or ch > '9':
+        is_digit = False
+        break
+
+if is_digit:
+    print("String contains only digits")
+else:
+    print("String does not contain only digits")
